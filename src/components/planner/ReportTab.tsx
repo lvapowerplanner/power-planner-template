c5004811c938dc61d0c2a6f46d19f59215f2be3c
Alex Barrett <alexpbarrett95@gmail.com>
2026-06-17 09:36:49 +0100

    Report column changes V2

--- src/components/planner/ReportTab.tsx
+++ src/components/planner/ReportTab.tsx
@@ -511,8 +511,8 @@ function DistroReport({
 
       <table style={styles.table}>
         <colgroup>
-          <col style={{ width: "17%" }} />
-          <col style={{ width: "4%" }} />
+          <col style={{ width: "15%" }} />
+          <col style={{ width: "6%" }} />
           <col style={{ width: "10%" }} />
           <col style={{ width: "24%" }} />
           <col style={{ width: "6%" }} />
